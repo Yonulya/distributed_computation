6c36783d8772f494b90bdc20297c935163662575
Yonulya <paul.colinmaire@viacesi.fr>
2025-12-12 02:15:30 +0100

    trying to facilitate python installation

---
+++ upgrade_ubuntu_python.sh
@@ -0,0 +1,7 @@
+sudo add-apt-repository ppa:deadsnakes/ppa -y
+sudo apt update
+sudo apt install python3.13 python3.13-venv -y
+python3.13 -m venv .venv
+source .venv/bin/activate
+pip install --upgrade pip
+pip install -r ./dask/master/worker/requirements.txt
