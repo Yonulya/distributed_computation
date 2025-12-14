bb8d97f460c9bf33183d2976d8825a1ed2b1435b
Yonulya <paul.colinmaire@viacesi.fr>
2025-12-14 20:59:30 +0100

    fix bug

--- src/distributed_computation/worker/tasks.py
+++ src/distributed_computation/worker/tasks.py
@@ -11,7 +11,7 @@ setup_logging()
 logger = logging.getLogger(__name__)
 
 # Run in parallel using ThreadPoolExecutor
-def ping_multiple_websites_task(urls: list[str], max_workers: int) -> list[tuple[str, str]]:
+def ping_multiple_websites_task(urls: list[str]) -> list[tuple[str, str]]:
 
     with ThreadPoolExecutor(max_workers=min(32, len(urls))) as executor:
         results = list(executor.map(ping_website, urls))
